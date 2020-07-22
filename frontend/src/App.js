30f57fd06ac54ac47d691c906ba92c38388e4a06
Salma Milla Gallegos <salma_mgs@live.com.mx>
2020-07-21 19:20:39 -0500

    Add frontend files

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom' 
+
+import {Menu} from './components/Menu'
+import {DefaultFilter} from './components/DefaultFilter'
+import {UserFilters} from './components/UserFilters'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/menu" component={Menu} />
+        <Route path="/default-filter" component={DefaultFilter} />
+        <Route path="/user-filters" component={UserFilters} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
